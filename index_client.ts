606b5781b8da8a6cbebbe878cee620cb9a040134
fluk <flukkulf2538@gmail.com>
2024-10-04 12:09:45 +0700

    with client stub

--- index_client.ts
+++ index_client.ts
@@ -1,31 +1,22 @@
-import axios from 'axios'
-import type { RPCRequest, RPCResponse } from './src/types/rpc'
+import { RPCClientStub } from './src/models/RPCClientStub'
 
-async function callRPC(method: string, params: any[]): Promise<any> {
-  const rpcRequest: RPCRequest = { method, params }
-
-  try {
-    const response = await axios.post<RPCResponse>('http://localhost:3000/rpc', rpcRequest)
-    if (response.data.error) {
-      throw new Error(response.data.error)
-    }
-    return response.data.result
-  }
-  catch (error) {
-    console.error('RPC Error:', error)
-    throw error
-  }
-}
-
+// Usage
 (async () => {
+  const rpcClient = new RPCClientStub('http://localhost:3000/rpc')
+
   try {
-    const sum = await callRPC('add', [5, 3])
+    const sum = await rpcClient.add(5, 3)
     console.log('Sum:', sum) // Sum: 8
 
-    const difference = await callRPC('subtract', [10, 4])
+    const difference = await rpcClient.subtract(10, 4)
     console.log('Difference:', difference) // Difference: 6
   }
   catch (error) {
-    console.error('Error occurred:', (error as Error).message)
+    if (error instanceof Error) {
+      console.error('Error occurred:', error.message)
+    }
+    else {
+      console.error('Unexpected error:', error)
+    }
   }
 })()
